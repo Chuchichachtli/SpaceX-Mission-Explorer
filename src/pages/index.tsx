9d94d9f6eb6337541870008e0c4d65f66e29cabf
barisbasmak <barisbasmak@gmail.com>
2021-12-24 18:56:01 +0300

    Main Page api call added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,105 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './index.css';
-import { Input, InputNumber, Card } from 'antd';
-const { Search } = Input ;
-export default function() {
+import { Input, Card, Row, Col } from 'antd';
+const { Search } = Input;
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-  const defaultResultCount:number = 10;
+export default function () {
+
+  const client = new ApolloClient({
+    uri: 'https://api.SpaceX.land/graphql',
+    cache: new InMemoryCache()
+  });
+
+  const defaultResultCount: number = 10;
   const [resultCount, setResultCount] = useState(defaultResultCount);
   const [missionName, setMissionName] = useState("");
+  // const [results, SetResults]
+  const [showResults, SetShowResults] = useState(false);
 
   const onSearch = () => {
-    console.log(missionName +" "+ resultCount);
+    client.query({
+      query: gql`
+      {
+        launchesPast(limit: 10, find: {mission_name: "${missionName}"}) {
+          mission_name
+          launch_site {
+            site_name_long
+            site_name
+          }
+          links {
+            article_link
+          }
+          launch_date_local
+          launch_date_unix
+          launch_success
+        }
+      }
+
+    `
+    }).then(result => console.log(result));
   }
   const onTextChange = (e: React.FormEvent<HTMLInputElement>) => {
     setMissionName(e.currentTarget.value);
   }
 
-  const onCountChange = (e : React.FormEvent<HTMLInputElement>) => {
+  const onCountChange = (e: React.FormEvent<HTMLInputElement>) => {
     let val = e.currentTarget.value;
     val = val.replace(/[a-z][A-Z]\s/, "");
-    if (val !== ""){
-      console.log(val, typeof(val), val.length, parseInt(val));
+    if (val !== "") {
+      console.log(val, typeof (val), val.length, parseInt(val));
 
       let newValue = parseInt(val);
       console.log(newValue);
       setResultCount(newValue);
-    }else if(val.length == 0  ){
-      setResultCount(0);
-    }else{
+    } else {
       setResultCount(0)
     }
-
+  }
+  const renderResults = () => {
+    if (showResults) {
+      return <></>
+    } else {
+      return <Card className={styles.resultCard} title="Results">123 </Card>
+    }
   }
 
   return (
     <div className={styles.normal}>
+      <Row gutter={8} align='top'>
+        <Col span={4} pull={20}>
+          <Card bordered={true}
+            className={styles.searchCard}
+            title={<span style={{ position: "relative", marginLeft: "20px", fontSize: "20px" }}>Search</span>} >
+            <br />
+            <form onSubmit={onSearch}>
+              <Search className={styles.searchInput}
+                enterButton
+                onSearch={onSearch}
+                placeholder='Mission Name'
+                value={missionName}
+                onChange={onTextChange}
+              />
+              <Input className={styles.numberInput} defaultValue={defaultResultCount} value={resultCount}
+                onChange={onCountChange}></Input>
+            </form>
+          </Card>
+          sadsad
+        </Col>
 
-      <Card bordered={true}
-        className={styles.searchCard}
-        title={<span style={{position: "relative", marginLeft:"20px", fontSize:"20px"}}>Search</span>} >
-        <br/>
-        <form onSubmit={onSearch}>
-          <Search className={styles.searchInput}
-            enterButton
-            onSearch={onSearch}
-            placeholder='Mission Name'
-            value={missionName}
-            onChange={onTextChange}
-          />
-          <Input className={styles.numberInput} defaultValue={defaultResultCount} value={resultCount}
-           onChange={onCountChange}></Input>
-        </form>
-      </Card>
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
+
+        <Col span={16} >
+          {/* {renderResults()} */}
+          sadasd
+        </Col>
+
+      </Row>
     </div>
   );
 }
